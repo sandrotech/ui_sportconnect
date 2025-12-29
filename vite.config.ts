a2772445dc0326652bad2d6fdea0f40f328a8c23
Alessandro Barbosa <alessandro.santos@fagifor.fortaleza.ce.gov.br>
2025-12-29 17:06:43 -0300

    ajuste de bug do design

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: [
-      'sportconnect.alessandrosantos.dev'
-    ]
-  }
+  plugins: [react()]
 })
